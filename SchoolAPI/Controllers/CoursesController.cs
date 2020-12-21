22998c6a110f317b2d94c7fe79636886ef78da5c
Amal50 <smartprogs.com@gmail.com>
2020-12-20 23:19:53 -0500

    undo test

--- SchoolAPI/Controllers/CoursesController.cs
+++ SchoolAPI/Controllers/CoursesController.cs
@@ -15,7 +15,6 @@ namespace SchoolAPI.Controllers
         ApplicationDbContext _context;
         public CoursesController()
         {
-            //test
             _context = new ApplicationDbContext();
         }
 
